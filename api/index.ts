745e1852ecf02558b09b020a0785ba0ecde0a1f6
pixelonius <razvanplastoi@yahoo.com>
2025-12-09 18:58:01 +0200

    added vercel deploy info

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,10 @@
+
 import { app } from "../server/app";
 import { registerRoutes } from "../server/routes";
 
-// Register routes on the app instance
-// Note: registerRoutes returns a promise of the HTTP server, 
-// but we only need the side effect of routes being registered on 'app'.
-registerRoutes(app);
+// Initialize routes
+// Note: We use top-level await which is supported in Node.js modules and Vercel
+await registerRoutes(app);
 
-// Export the app for Vercel
+// Export the app for Vercel Serverless Functions
 export default app;
